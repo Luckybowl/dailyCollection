7ac92eea882618810ac23d32284bd4d290688dc1
chenpeng <chenpeng@primeton.com>
2018-03-29 09:36:08 +0800

    Update:更新注释。

--- Test1.java
+++ Test1.java
@@ -21,7 +21,7 @@ public class Test1 {
         	if(!a.equals(x)){
         		iter.remove(); //若用list.remove(x),则会报ConcurrentModificationException，因为元素在使用的时候发生了并发的修改，导致异常抛出。
 //        		list.remove(x);
-        		
+/**********update:2018-3-29 09:22:50 不推荐在迭代器中删除，可以遍历符合条件的对象，然后塞入一个新的list。java 8中可以使用filter方法。************************************ */
         	}
         	
         }
