d25af8aff08432a532b72b274cbd4affc5445dcc
cp <cp502966686@outlook.com>
2018-07-04 15:24:56 +0800

    Add:可以用hash来做，更简单。

---
+++ SumOfTwoNum.java
@@ -0,0 +1,53 @@
+
+public class SumOfTwoNum{
+        static int temp;
+        public static void main(String[] args){
+            int[] nums ={2, 7, 11, 15,3,8,5};
+            int target = 15;
+            nums = twoSum(nums,target);
+            System.out.print(nums);
+        }
+        public static int[] twoSum(int[] nums, int target) {
+            int[] result = new int[2];
+            nums = BubbleSort(nums);
+            nums = arraycopy(nums,target);
+            for(int i = 0 ; i < nums.length; i ++){
+                for(int j = i+1 ; j < nums.length ; j++ ){
+                    if(nums[i] + nums[j] == target){
+                        result[0] = nums[i];
+                        result[1] = nums[j];
+                        return result;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int[] BubbleSort(int[] nums){
+
+            for(int i = 0 ; i < nums.length ; i ++){
+                for(int j = i+1 ; j < nums.length; j++ ){
+                    if(nums[i] > nums[j]){
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return nums;
+        }
+
+        public static int[] arraycopy(int[] nums,int target){
+            int x = 0;
+            int[] b = new int[nums.length];
+            for(int i = 0 ; i < nums.length; i ++){
+                if(nums[i] >= target){
+                    x = i;
+                    break;
+                }
+            }
+            System.arraycopy(nums,0,b,0,x);
+            return b;
+        }
+    
+}
